7df52216fc9170389326cac100a8f7cce0e06b72
dakaca007 009 <1014458152@qq.com>
2023-06-17 02:45:44 +0000

    ok

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -96,10 +96,8 @@ export function SideBar(props: { className?: string }) {
       }`}
     >
       <div className={styles["sidebar-header"]}>
-        <div className={styles["sidebar-title"]}>ChatGPT Next</div>
-        <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
-        </div>
+        <div className={styles["sidebar-title"]}>犀牛哥</div>
+        <div className={styles["sidebar-sub-title"]}>新的世界即将到来！</div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
         </div>
